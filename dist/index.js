3d1a74b016e986889301c6ae260edb40d1a31b72
Matthew Coleman <matthewdecoleman@gmail.com>
2019-12-13 10:59:30 +0000

    Updated: dist

--- dist/index.js
+++ dist/index.js
@@ -496,7 +496,38 @@ AWS.SharedIniFileCredentials = AWS.util.inherit(AWS.Credentials, {
 /***/ 31:
 /***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
-const run = __webpack_require__(430);
+const core = __webpack_require__(470);
+const {
+  createChangeSet,
+  deleteChangeSet,
+  executeChangeSet
+} = __webpack_require__(46);
+
+async function run() {
+  try {
+    const METHOD = core.getInput("method");
+
+    switch (METHOD) {
+      case "create":
+        const { id, name } = await createChangeSet();
+
+        core.setOutput("changeset_id", id);
+        core.setOutput("changeset_name", name);
+        break;
+      case "delete":
+        await deleteChangeSet();
+        break;
+      case "execute":
+        await executeChangeSet();
+        break;
+      default:
+        core.setFailed(`method ${METHOD} not supported`);
+    }
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
 
 run();
 
@@ -674,6 +705,105 @@ AWS.TemporaryCredentials = AWS.util.inherit(AWS.Credentials, {
   }
 
 });
+
+
+/***/ }),
+
+/***/ 46:
+/***/ (function(module, __unusedexports, __webpack_require__) {
+
+const { existsSync, readFileSync } = __webpack_require__(747);
+const CloudFormation = __webpack_require__(21);
+const core = __webpack_require__(470);
+const uuidV4 = __webpack_require__(826);
+
+async function createChangeSet() {
+  const STACK_NAME = core.getInput("stack_name");
+  const TEMPLATE_FILE = core.getInput("template_file");
+  const AWS_ACCESS_KEY_ID = core.getInput("aws_access_key_id");
+  const AWS_SECRET_ACCESS_KEY = core.getInput("aws_secret_access_key");
+  const AWS_REGION = core.getInput("aws_region");
+  const PARAMETERS = core.getInput("parameters");
+
+  if (existsSync(TEMPLATE_FILE)) {
+    const file = await readFileSync(TEMPLATE_FILE);
+
+    const cfn = new CloudFormation({
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+      region: AWS_REGION
+    });
+
+    let params = {
+      Capabilities: ["CAPABILITY_IAM"],
+      ChangeSetType: "CREATE",
+      ChangeSetName: `${STACK_NAME}-${uuidV4()}`,
+      StackName: STACK_NAME,
+      TemplateBody: file.toString(),
+      Parameters: []
+    };
+
+    if (PARAMETERS && PARAMETERS.trim() !== "") {
+      const keyValues = PARAMETERS.split(",");
+
+      keyValues.forEach(kv => {
+        const values = kv.split("=");
+        params.Parameters.push({
+          ParameterKey: values[0],
+          ParameterValue: values[1]
+        });
+      });
+    }
+
+    const { Id } = await cfn.createChangeSet(params).promise();
+
+    return { id: Id, name: params.ChangeSetName };
+  } else {
+    throw new Error(`${TEMPLATE_FILE} not found`);
+  }
+}
+
+async function executeChangeSet() {
+  const STACK_NAME = core.getInput("stack_name");
+  const CHANGESET_NAME = core.getInput("changeset_name");
+  const AWS_ACCESS_KEY_ID = core.getInput("aws_access_key_id");
+  const AWS_SECRET_ACCESS_KEY = core.getInput("aws_secret_access_key");
+  const AWS_REGION = core.getInput("aws_region");
+
+  const cfn = new CloudFormation({
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    region: AWS_REGION
+  });
+
+  await cfn
+    .executeChangeSet({ ChangeSetName: CHANGESET_NAME, StackName: STACK_NAME })
+    .promise();
+}
+
+async function deleteChangeSet() {
+  const STACK_NAME = core.getInput("stack_name");
+  const CHANGESET_NAME = core.getInput("changeset_name");
+  const AWS_ACCESS_KEY_ID = core.getInput("aws_access_key_id");
+  const AWS_SECRET_ACCESS_KEY = core.getInput("aws_secret_access_key");
+  const AWS_REGION = core.getInput("aws_region");
+
+  const cfn = new CloudFormation({
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    region: AWS_REGION
+  });
+
+  await cfn
+    .deleteChangeSet({ ChangeSetName: CHANGESET_NAME, StackName: STACK_NAME })
+    .promise();
+}
+
+module.exports = {
+  createChangeSet,
+  deleteChangeSet,
+  executeChangeSet
+};
 
 
 /***/ }),
@@ -4433,82 +4563,6 @@ AWS.RemoteCredentials = AWS.util.inherit(AWS.Credentials, {
     });
   }
 });
-
-
-/***/ }),
-
-/***/ 430:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-const core = __webpack_require__(470);
-const CloudFormation = __webpack_require__(21);
-const { existsSync, readFileSync } = __webpack_require__(747);
-const uuidV4 = __webpack_require__(826);
-
-async function create() {
-  const STACK_NAME = core.getInput("stack_name");
-  const TEMPLATE_FILE = core.getInput("template_file");
-  const AWS_ACCESS_KEY_ID = core.getInput("aws_access_key_id");
-  const AWS_SECRET_ACCESS_KEY = core.getInput("aws_secret_access_key");
-  const AWS_REGION = core.getInput("aws_region");
-  const PARAMETERS = core.getInput("parameters");
-
-  if (existsSync(TEMPLATE_FILE)) {
-    const file = await readFileSync(TEMPLATE_FILE);
-
-    const cfn = new CloudFormation({
-      accessKeyId: AWS_ACCESS_KEY_ID,
-      secretAccessKey: AWS_SECRET_ACCESS_KEY,
-      region: AWS_REGION
-    });
-
-    let params = {
-      Capabilities: ["CAPABILITY_IAM"],
-      ChangeSetType: "CREATE",
-      ChangeSetName: `${STACK_NAME}-${uuidV4()}`,
-      StackName: STACK_NAME,
-      TemplateBody: file.toString(),
-      Parameters: []
-    };
-
-    if (PARAMETERS && PARAMETERS.trim() !== "") {
-      const keyValues = PARAMETERS.split(",");
-
-      keyValues.forEach(kv => {
-        const values = kv.split("=");
-        params.Parameters.push({
-          ParameterKey: values[0],
-          ParameterValue: values[1]
-        });
-      });
-    }
-
-    const response = await cfn.createChangeSet(params).promise();
-
-    core.setOutput("changeset_id", response.Id);
-  } else {
-    core.setFailed(`${TEMPLATE_FILE} not found`);
-  }
-}
-
-async function run() {
-  try {
-    const METHOD = core.getInput("method");
-
-    switch (METHOD) {
-      case "create":
-        create();
-        break;
-      default:
-        core.setFailed(`method ${METHOD} not supported`);
-    }
-  } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
-  }
-}
-
-module.exports = run;
 
 
 /***/ }),
